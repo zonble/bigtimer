4741648e97cf132e8bc2a81dffae9ed9436e125b
Weizhong Yang <zonble@gmail.com>
2009-03-07 00:33:32 +0800

    Starts the BigTimer project.

---
+++ BigTimer/BigTimer.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace BigTimer
+{
+    partial class BigTimer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.u_start = new System.Windows.Forms.Button();
+            this.u_stop = new System.Windows.Forms.Button();
+            this.u_zero = new System.Windows.Forms.Button();
+            this.u_show = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // u_start
+            // 
+            this.u_start.Font = new System.Drawing.Font("PMingLiU", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.u_start.Location = new System.Drawing.Point(12, 84);
+            this.u_start.Name = "u_start";
+            this.u_start.Size = new System.Drawing.Size(355, 52);
+            this.u_start.TabIndex = 0;
+            this.u_start.Text = "開始";
+            this.u_start.UseVisualStyleBackColor = true;
+            this.u_start.Click += new System.EventHandler(this.u_start_Click);
+            // 
+            // u_stop
+            // 
+            this.u_stop.Font = new System.Drawing.Font("PMingLiU", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.u_stop.Location = new System.Drawing.Point(12, 142);
+            this.u_stop.Name = "u_stop";
+            this.u_stop.Size = new System.Drawing.Size(355, 52);
+            this.u_stop.TabIndex = 1;
+            this.u_stop.Text = "停止";
+            this.u_stop.UseVisualStyleBackColor = true;
+            this.u_stop.Click += new System.EventHandler(this.u_stop_Click);
+            // 
+            // u_zero
+            // 
+            this.u_zero.Font = new System.Drawing.Font("PMingLiU", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.u_zero.Location = new System.Drawing.Point(12, 200);
+            this.u_zero.Name = "u_zero";
+            this.u_zero.Size = new System.Drawing.Size(355, 52);
+            this.u_zero.TabIndex = 2;
+            this.u_zero.Text = "歸零";
+            this.u_zero.UseVisualStyleBackColor = true;
+            this.u_zero.Click += new System.EventHandler(this.u_zero_Click);
+            // 
+            // u_show
+            // 
+            this.u_show.AutoSize = true;
+            this.u_show.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.u_show.Location = new System.Drawing.Point(12, 9);
+            this.u_show.Name = "u_show";
+            this.u_show.Size = new System.Drawing.Size(372, 72);
+            this.u_show.TabIndex = 3;
+            this.u_show.Text = "00:00:00.00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 269);
+            this.Controls.Add(this.u_show);
+            this.Controls.Add(this.u_zero);
+            this.Controls.Add(this.u_stop);
+            this.Controls.Add(this.u_start);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "大計時器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button u_start;
+        private System.Windows.Forms.Button u_stop;
+        private System.Windows.Forms.Button u_zero;
+        private System.Windows.Forms.Label u_show;
+    }
+}
+
---
+++ BigTimer/BigTimer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BigTimer
+{
+    public partial class BigTimer : Form
+    {
+        DateTime m_begin;
+		TimeSpan m_totalDuration;
+        Timer m_timer = new Timer();
+        
+        public BigTimer()
+        {
+            InitializeComponent();
+            u_stop.Enabled = false;
+            u_zero.Enabled = false;
+            m_timer = new Timer();
+            m_timer.Interval = 100;
+            m_timer.Tick += new EventHandler(m_timer_Tick);
+        }
+
+        void m_timer_Tick(object sender, EventArgs e)
+        {
+            // throw new Exception("The method or operation is not implemented.");
+            DateTime now = DateTime.Now;
+            TimeSpan duration = now - m_begin;
+			m_begin = now;
+			m_totalDuration += duration;
+
+            string h;
+			if (m_totalDuration.Hours < 10)
+				h = "0" + m_totalDuration.Hours;
+            else
+				h = "" + m_totalDuration.Hours;
+
+            string m;
+			if (m_totalDuration.Minutes < 10)
+				m = "0" + m_totalDuration.Minutes;
+            else
+				m = "" + m_totalDuration.Minutes;
+
+            string s;
+			if (m_totalDuration.Seconds < 10)
+				s = "0" + m_totalDuration.Seconds;
+            else
+				s = "" + m_totalDuration.Seconds;
+
+            string ms;
+			int msInt = m_totalDuration.Milliseconds / 10;
+            if (msInt < 10)
+                ms = "0" + msInt;
+            else
+                ms = "" + msInt;
+
+            u_show.Text = h + ":" + m + ":" + s + "." + ms;
+        }
+
+        private void u_start_Click(object sender, EventArgs e)
+        {
+            m_begin = DateTime.Now;
+            m_timer.Start();
+            u_stop.Enabled = true;
+            u_start.Enabled = false;
+            u_zero.Enabled = false;
+        }
+
+        private void u_stop_Click(object sender, EventArgs e)
+        {
+            m_timer.Stop();
+            u_stop.Enabled = false;
+            u_start.Enabled = true;
+            u_zero.Enabled = true;
+        }
+
+        private void u_zero_Click(object sender, EventArgs e)
+        {
+            u_show.Text = "00:00:00.00";
+			m_totalDuration = TimeSpan.Zero;
+            u_zero.Enabled = false;
+        }
+    }
+}
